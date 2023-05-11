f83db19848779dda6eac2bbd12e82863ae6c3bfb
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-11 20:29:35 +0700

    new file:   pages/api/test.ts

---
+++ pages/api/test.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await new Promise((res, rej) => {
+    setTimeout(() => res('hello'), 15000)
+  })
+  
+  res.status(200).json({ name: 'John Doe' })
+}
